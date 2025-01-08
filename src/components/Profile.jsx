758123704cfef8a0af7e228dbf56c53c2bdd98b6
Woody <waiyanwoody@gmail.com>
2025-01-08 23:44:51 +0630

    update:

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,7 +2,7 @@
 const Profile = () => {
   return (
     <div className='w-16 h-16 bg-slate-200 border-2 border-emerald-500 rounded-full overflow-hidden'>
-      <img src='/img/shun-2.jpg' className="" />
+      <img src="./img/shun-2.jpg" className="" alt="Profile" />
     </div>
   )
 }
